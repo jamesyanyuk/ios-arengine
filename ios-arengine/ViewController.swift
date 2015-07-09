55e9e0376824e578596afafceacb6471ae4b7ae1
James Yanyuk <jayanyuk@cisco.com>
2015-07-09 10:51:47 -0400

    Initial Commit

---
+++ ios-arengine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-arengine
+//
+//  Created by jayanyuk on 7/9/15.
+//  Copyright (c) 2015 Placemark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
